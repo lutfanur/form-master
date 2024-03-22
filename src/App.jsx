c039963679aa0ea9338fddd14a38688d1028a64a
Lutfaakter <lutfanur20@gmail.com>
2024-03-22 00:52:32 +0000

    added reusable form using props and children

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './RefForm/RefForm'
 // import StatefulForm from './components/statefulForm/StatefulForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
+  const handleSignUpSubmit = data => {
+  console.log('SignUp data', data);
+  }
+
+  const handleUpdateProfile = data => {
+  console.log('Update profile', data)
+  }
 
   return (
     <>
@@ -13,7 +21,24 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm
+        formTitle={'Sign Up'}
+        handleSubmit={handleSignUpSubmit}>
+          <div>
+            <h2>Sign Up</h2>
+            <p>Please Signup right now</p>
+          </div>
+        </ReusableForm>
+      <ReusableForm
+        formTitle={'Profile Update'}
+        handleSubmit={handleUpdateProfile}
+        submitBtnText='Update'>
+          <div>
+            <h2>Update Profile</h2>
+            <p>Always keep your profile updated</p>
+          </div>
+        </ReusableForm>
     </>
   )
 }
