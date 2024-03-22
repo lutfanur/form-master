224bca9315f563bcd5598cb2fa8d88654cd0dc0b
Lutfaakter <lutfanur20@gmail.com>
2024-03-22 17:58:13 +0000

    added how to use context API to avoid prop drilling

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,23 +1,29 @@
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Father from "../Father/Father";
 import Uncle from "../Uncle/Uncle";
 import './Grandpa.css'
 
-const AssetContext = createcontext('gold');
+export const AssetContext = createContext('gold');
+export const MoneyContext = createContext(1000);
 
 
 const Grandpa = () => {
+    const [money, setMoney] = useState(1000);
     const asset = 'diamond'
     return (
         <div className="grandpa">
             <h2>Grandpa</h2>
-            <AssetContext.Provider value="gold">
-                <section className="flex">
-                    <Father asset={asset}></Father>
-                    <Uncle asset={asset}></Uncle>
-                    <Aunty></Aunty>
-                </section>
-            </AssetContext.Provider>
+            <p>Net Money: {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <AssetContext.Provider value="gold">
+                    <section className="flex">
+                        <Father asset={asset}></Father>
+                        <Uncle asset={asset}></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </AssetContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
